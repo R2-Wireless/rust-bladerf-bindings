d11a6bb2831e62da02bd890dfc09233400cb0ff9
Yuval Saraf <unimonkiez@gmail.com>
2023-10-27 21:33:38 +0300

    Change git clone to be in out dir

--- build.rs
+++ build.rs
@@ -2,21 +2,26 @@ use std::process::Command;
 
 fn main() {
     println!("cargo:rerun-if-changed=lib-tag");
+    let out_dir = std::path::PathBuf::from(std::env::var("OUT_DIR").unwrap());
     let lib_tag =
         std::fs::read_to_string("lib-tag").expect("Should have been able to read the file");
     let lib_tag = lib_tag.trim();
-    let _ = Command::new("rm").args(["-rf", "bladeRF"]).output();
+    let _ = Command::new("rm")
+        .current_dir(&out_dir)
+        .args(["-rf", "bladeRF"])
+        .output();
     let _ = Command::new("mkdir")
+        .current_dir(&out_dir)
         .args(["bladeRF"])
         .output()
         .expect("Wasn't able to build bladeRF");
     let _ = Command::new("git")
-        .current_dir("bladeRF")
+        .current_dir(out_dir.join("bladeRF"))
         .args(["init"])
         .output()
         .expect("Wasn't able to build bladeRF");
     let _ = Command::new("git")
-        .current_dir("bladeRF")
+        .current_dir(out_dir.join("bladeRF"))
         .args([
             "remote",
             "add",
@@ -26,33 +31,42 @@ fn main() {
         .output()
         .expect("Wasn't able to build bladeRF");
     let _ = Command::new("git")
-        .current_dir("bladeRF")
+        .current_dir(out_dir.join("bladeRF"))
         .args(["fetch", "--depth", "1", "origin", lib_tag])
         .output()
         .expect("Wasn't able to build bladeRF");
     let _ = Command::new("git")
-        .current_dir("bladeRF")
+        .current_dir(out_dir.join("bladeRF"))
         .args(["checkout", "FETCH_HEAD"])
         .output()
         .expect("Wasn't able to build bladeRF");
 
     let _ = Command::new("mkdir")
-        .current_dir("bladeRF/host")
+        .current_dir(out_dir.join("bladeRF").join("host"))
         .args(["-p", "build"])
         .output()
         .expect("Wasn't able to build bladeRF");
     let _ = Command::new("cmake")
-        .current_dir("bladeRF/host/build")
+        .current_dir(out_dir.join("bladeRF").join("host").join("build"))
         .args(["../"])
         .output()
         .expect("Wasn't able to build bladeRF");
     let _ = Command::new("make")
-        .current_dir("bladeRF/host/build")
+        .current_dir(out_dir.join("bladeRF").join("host").join("build"))
         .output()
         .expect("Wasn't able to build bladeRF");
 
-    println!("cargo:rustc-env=DYLD_LIBRARY_PATH=./bladeRF/host/build/output");
-    println!("cargo:rustc-env=LD_LIBRARY_PATH=./bladeRF/host/build/output");
-    println!("cargo:rustc-link-search=./bladeRF/host/build/output");
+    let lib_output = out_dir
+        .join("bladeRF")
+        .join("host")
+        .join("build")
+        .join("output");
+    let lib_output = match lib_output.to_str() {
+        Some(x) => x,
+        _ => panic!("Should exist"),
+    };
+    println!("cargo:rustc-env=DYLD_LIBRARY_PATH={}", &lib_output);
+    println!("cargo:rustc-env=LD_LIBRARY_PATH={}", &lib_output);
+    println!("cargo:rustc-link-search={}", &lib_output);
     println!("cargo:rustc-link-lib=bladeRF");
 }
--- codegen.rs
+++ codegen.rs
@@ -1,14 +1,25 @@
 extern crate bindgen;
+extern crate cargo_metadata;
 
-use std::env;
-use std::path::PathBuf;
+use cargo_metadata::{Message, MetadataCommand};
+use std::error::Error;
+use std::io::BufReader;
+use std::process::{Command, Stdio};
 
 fn main() {
+    let out_dir = get_out_dir().expect("Couldn't find lib dir");
+    let header_dir = out_dir
+        .join("bladeRF")
+        .join("host")
+        .join("libraries")
+        .join("libbladeRF")
+        .join("include");
+    let header_dir = header_dir.to_str().unwrap();
     let bindings = bindgen::Builder::default()
         // The input header we would like to generate
         // bindings for.
         .header("bindings.h")
-        .clang_arg("-I./bladeRF/host/libraries/libbladeRF/include")
+        .clang_arg(format!("-I{}", &header_dir))
         // Tell cargo to invalidate the built crate whenever any of the
         // included header files changed.
         .parse_callbacks(Box::new(bindgen::CargoCallbacks))
@@ -18,8 +29,34 @@ fn main() {
         .expect("Unable to generate bindings");
 
     // Write the bindings to the $OUT_DIR/bindings.rs file.
-    let out_path = PathBuf::from("./src");
+    let out_path = std::path::PathBuf::from("./src");
     bindings
         .write_to_file(out_path.join("bindings.rs"))
         .expect("Couldn't write bindings!");
+}
+
+fn get_out_dir() -> Result<std::path::PathBuf, Box<dyn Error>> {
+    let metadata = MetadataCommand::new().exec()?;
+    let package = match metadata.root_package() {
+        Some(p) => p,
+        None => return Err("cargo out-dir must be run from within a crate".into()),
+    };
+    let mut command = Command::new("cargo")
+        .args(&["check", "--message-format=json", "--quiet"])
+        .stdout(Stdio::piped())
+        .stderr(Stdio::null())
+        .spawn()
+        .unwrap();
+    let reader = BufReader::new(command.stdout.take().unwrap());
+    for message in Message::parse_stream(reader) {
+        match message? {
+            Message::BuildScriptExecuted(script) => {
+                if script.package_id == package.id {
+                    return Ok(script.out_dir);
+                }
+            }
+            _ => (),
+        }
+    }
+    Err(format!("crate {} did not run a build script", package.name).into())
 }
